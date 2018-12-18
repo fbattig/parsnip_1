cab323ee10afaf75c1eae53fa6e996d0950b2dc0
Felix Battig <fbattig@yahoo.com>
2018-12-18 13:58:09 -0500

    backup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TasksPages from './components/TasksPage';
 import './App.css';
+
+const mockTasks = [
+  {
+    id: 1,
+    title: 'Learn Redux',
+    description: 'store, actions, reducers,,,',
+    status: 'In Progress'
+  },
+  {
+    id: 2,
+    title: 'Learn React',
+    description: 'local state props',
+    status: 'Completed'
+  },
+  {
+    id: 3,
+    title: 'Learn Router',
+    description: 'Router, NavLik..',
+    status: 'Unstarted'
+  },
+  {
+    id: 4,
+    title: 'Learn Tableau',
+    description: 'BI..',
+    status: 'Unstarted'
+  }
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <TasksPages tasks={mockTasks} />
       </div>
     );
   }
---
+++ src/components/TaskList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TaskList = (props) => {
+  return (
+    <div>
+     {console.log(props)}
+      </div>)
+}
+
+export default TaskList;
